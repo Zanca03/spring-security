fcbed0ab7f7808de6f9f59afefa59773218b151d
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-03-29 19:29:53 -0300

    Adding InMemoryUserDetailManager with a fix user

--- src/main/java/dev/carlosezpereira/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/dev/carlosezpereira/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package dev.carlosezpereira.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +17,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","INDEX","/css/*","/js/*")
+                .antMatchers("/", "INDEX", "/css/*", "/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails usuario = User.builder()
+                .username("Carlos")
+                .password("coxinha123")
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(usuario);
     }
 }
