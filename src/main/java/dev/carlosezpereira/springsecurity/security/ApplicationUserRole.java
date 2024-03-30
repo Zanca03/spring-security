05207be065a5b3dc65ee830591fdcc755ff258e4
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-03-29 21:53:44 -0300

    Create permissions and roles with Enums

---
+++ src/main/java/dev/carlosezpereira/springsecurity/security/ApplicationUserRole.java
@@ -0,0 +1,17 @@
+package dev.carlosezpereira.springsecurity.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static dev.carlosezpereira.springsecurity.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions){
+        this.permissions = permissions;
+    }
+}
