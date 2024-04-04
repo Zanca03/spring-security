77eff7f3dae36835b1a213d66f47b8d0c0a5a478
Carlos Eduardo Zancanella Pereira <carloszanca03@gmail.com>
2024-04-04 07:23:08 -0300

    Add permissions to use HTTP methods based on permission role

--- src/main/java/dev/carlosezpereira/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/dev/carlosezpereira/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package dev.carlosezpereira.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static dev.carlosezpereira.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static dev.carlosezpereira.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -25,9 +28,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "INDEX", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/managment/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/managment/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/managment/api/**").hasAnyAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/managment/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,8 +53,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails usuarioAdm = User.builder()
                 .username("Admin")
                 .password(passwordEncoder.encode("root"))
-                .roles(ADMIN.name())
+                .roles(ADMIN.name()) //ROLE_ADMIN
                 .build();
-        return new InMemoryUserDetailsManager(usuarioComum,usuarioAdm);
+        UserDetails usuarioAdmTrainee = User.builder()
+                .username("AdminTrainee")
+                .password(passwordEncoder.encode("root"))
+                .roles(ADMINTRAINEE.name()) //ROLE_ADMIN
+                .build();
+        return new InMemoryUserDetailsManager(
+                usuarioComum,
+                usuarioAdm,
+                usuarioAdmTrainee
+        );
     }
 }
